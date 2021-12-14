8fb61adcebd566a05f7950670ceafbb53298f37a
Ken Ovienadu <kenovienadu@gmail.com>
2021-12-14 10:46:09 +0100

    feat: create initial modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApiModule } from './api/api.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ApiModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
